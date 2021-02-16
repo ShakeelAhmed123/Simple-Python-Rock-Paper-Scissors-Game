223b4572b24e41a01992f1b551c1961b26bed65e
Shakeel Ahmed Khan <76478495+ShakeelAhmed123@users.noreply.github.com>
2021-02-16 13:47:36 +0500

    Complete overhaul of code

--- Python-Simple Rock Paper Scissors/main.py
+++ Python-Simple Rock Paper Scissors/main.py
@@ -1,48 +1,60 @@
 import random
 
-print("WELCOME TO A GAME OF ROCK PAPER SCISSORS")
-print()
+# Welcome
+# Input Players move
+# Generate Random Counter Move
+# Compare Answers
+# Get Winner
+# Display Result
 
-operations = ["R", "P", "S", "ROCK", "PAPER", "SCISSOR"]
-print("[R]ock, [P]aper or [S]cissors?")
-move = input(">").upper()
+def main():
+	while True:
+		print('WELCOME TO A GAME OF ROCK PAPER SCISSORS\n')
 
-def game():
-    moves = ["ROCK", "PAPER", "SCISSOR"]
-    pick_move = random.choice(moves)
-    print(f"You picked {move} and I picked {pick_move}")
+		move_list = ['Rock', 'Paper', 'Scissor']
+		ai_choice = random.choice(move_list)
+		ai_choice_lower = str(ai_choice.lower())
 
-    if pick_move == "ROCK":
-        if move == "ROCK" or "R":
-            print("That's a tie! Let's retry!")
-        elif move == "SCISSORS" or "S":
-            print("I won! Yayyy!")
-        elif move == "PAPER" or "P":
-            print("You Won! Congrats!")
-        else:
-            print("Incorrect Move, Please Select [R]ock, [P]aper or [S]cissors!")
+		player_move = input("Rock, Paper or Scissor? >")
+		player_move = player_move.strip()
+		player_move_lower = player_move.lower()
+		player_move_lower = str(player_move_lower)
 
-    elif pick_move == "PAPER":
-        if move == "ROCK" or "R":
-            print("I won! Yayyy!")
-        elif move == "SCISSORS" or "S":
-            print("You Won! Congrats!")
-        elif move == "PAPER" or "P":
-            print("That's a tie! Let's retry!")
-        else:
-            print("Incorrect Move, Please Select [R]ock, [P]aper or [S]cissors!")
+		if player_move_lower == ai_choice_lower:
+			print("It's a draw!")
+			print(f'You both picked {player_move}')
 
-    elif pick_move == "SCISSOR":
-        if move == "ROCK" or "R":
-            print("You Won! Congrats!")
-        elif move == "SCISSORS" or "S":
-            print("That's a tie! Let's retry!")
-        elif move == "PAPER" or "P":
-            print("I won! Yayyy!")
-        else:
-            print("Incorrect Move, Please Select [R]ock, [P]aper or [S]cissors!")
+		elif player_move_lower == 'paper' and ai_choice_lower == 'rock':
+			print("AI Wins!")
+			print(f"You picked {player_move} and AI picked {ai_choice}")
 
-    else:
-        print("There seem to be some sort of error in the script!")
+		elif player_move_lower == 'paper' and ai_choice_lower == 'scissor':
+			print("AI Wins!")
+			print(f"You picked {player_move} and AI picked {ai_choice}")
 
-game()
+		elif player_move_lower == 'rock' and ai_choice_lower == 'paper':
+			print("AI Win!")
+			print(f"You picked {player_move} and AI picked {ai_choice}")
+
+		elif player_move_lower == 'rock' and ai_choice_lower == 'scissor':
+			print("You Win!")
+			print(f"You picked {player_move} and AI picked {ai_choice}")
+
+		elif player_move_lower == 'scissor' and ai_choice_lower == 'rock':
+			print("AI Win!")
+			print(f"You picked {player_move} and AI picked {ai_choice}")
+
+		elif player_move_lower == 'scissor' and ai_choice_lower == 'paper':
+			print("You Win!")
+			print(f"You picked {player_move} and AI picked {ai_choice}")
+
+		elif player_move_lower == 'quit' or 'exit':
+			exit()
+
+		else:
+			print('What did you just input?')
+
+		print('')
+
+if __name__ == '__main__':
+	main()
